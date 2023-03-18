7a40d151455047258044bb5f7393d254013bbd61
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-03-18 19:12:24 -0300

    create components css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,14 @@
+import styles from './Home.module.css';
+import savings from '../../assets/savings.svg'
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section> 
+      <h1>Bem-vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo</p>
+      <a href="/">Criar Projeto</a>
+      <img src={savings} alt="savings" />
+    </section>
   );
 }
 
