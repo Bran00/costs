c27ecd542cea92e0bf7ad7df78e992d504bebdb3
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-03-22 16:02:18 -0300

    create db with fakes datas and card of projects

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,41 @@
+import { useNavigate } from 'react-router-dom'
+
 import style from './NewProject.module.css';
 import ProjectForm from '../project/ProjectForm';
 
 function NewProject() {
+
+  const navigate = useNavigate();
+
+  function createPost(project) {
+    // initialize cost and services
+
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(project)
+    }
+  ).then(
+     (resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      navigate('/projects', { state: { message: 'Projeto criado com sucesso!' }});
+    })
+  .catch(err => console.log(err));
+}
+
   return (
     <div className={style.newProject_container}>
       <h1>Criar Projeto</h1>
       <p>Cria seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText="Criar projeto"/>
+      <ProjectForm 
+      handleSubmit={createPost}
+      btnText="Criar projeto"/>
     </div>
   )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,65 @@
+import { useLocation } from 'react-router-dom'
+
+import { useState, useEffect } from 'react'
+
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import Message from "../layout/Message"
+import ProjectCard from '../project/ProjectCard'
+
+import styles from './Projects.module.css'
+
 function Projects() {
-  return <h1>Projects</h1>
+  const [projects, setProjects] = useState([])
+  
+
+  const location = useLocation()
+  let message = ''
+  if (location.state) {
+    message = location.state.message
+  }
+
+  useEffect(() => {
+      fetch('http://localhost:5000/projects', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response =>
+        response.json())
+      .then((data) => {
+        setProjects(data)
+      })
+      .catch((err) => console.error(err))
+  }, [])
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <p>{
+            projects.length > 0 &&
+            projects.map((project) =>  project.name)
+          }</p>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
+      {message && <Message type="sucess" msg={message} />}
+      <Container customClass="start">
+        {
+        projects.length > 0 &&
+          projects.map((project) => 
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          )}
+      </Container>
+    </div>
+  )
 }
 
 export default Projects
